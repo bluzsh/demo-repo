9c8331d4e8975aca314c18ba42cea051ceb284b7
bluzsh <abdoulieybojangkb@gmail.com>
2023-01-27 08:44:35 +0100

    hello.c
    Adding code to hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+   char name[] = "Bluzsh";
+    int age = 20;
+    printf("Hello world! I'am %s, in my %d's \n", name,age);
+    return 0;
+}
